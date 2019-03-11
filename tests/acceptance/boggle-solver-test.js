f42dad164cce9b5db2317fe436b2c0f337b27697
Buck Doyle <b@chromatin.ca>
2019-03-10 19:16:44 -0500

    Add hard-coded answers

--- tests/acceptance/boggle-solver-test.js
+++ tests/acceptance/boggle-solver-test.js
@@ -1,15 +1,18 @@
 import { module, test } from 'qunit';
-import { click, fillIn, visit, currentURL } from '@ember/test-helpers';
+import { click, fillIn, find, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | boggle solver', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('entering a board navigates to the solver', async function(assert) {
+  test('entering a board shows found words', async function(assert) {
     await visit('/');
     await fillIn('input', 'ABCDEF');
     await click('button');
 
     assert.equal(currentURL(), '/ABCDEF');
+
+    assert.equal(find('#answers li:first-child').innerHTML, 'hello');
+    assert.equal(find('#answers li:last-child').innerHTML, 'hello again');
   });
 });
